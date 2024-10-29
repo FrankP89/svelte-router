9f5a2548563ea3b6eda3848e7b9faf7b20639f01
FrankP89 <walter.pintor@gmx.com>
2024-10-29 20:41:52 +0800

    55. Testing Routes

---
+++ routes/src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import Home from "./lib/pages/Home.svelte";
+  import Settings from "./lib/pages/Settings.svelte";
+  import { onMount } from "svelte";
+
+  let page;
+
+  function onRouteChange() {
+    const path = window.location.hash.slice(1);
+    if(path == "/"){
+      page = "home"
+    }else if(path == "/settings"){
+      page = "settings"
+    } else {
+      window.location.hash = "/";
+    }
+  }
+
+  onMount(onRouteChange);
+  
+</script>
+
+
+  
+<svelte:window on:hashchange={onRouteChange} />
+
+<nav>
+  <a href="#/">Home</a>
+  <a href="#/settings">Settings</a>
+</nav>
+
+{#if page === "home"}
+  <Home />
+{:else if page === "settings"}
+  <Settings />
+{/if}
+
+<style>
+</style>
